404947e7872415e59af7ec93845e42bae9abdbb8
HENRY-KC <henrykc24@yahoo.com>
2021-09-28 19:38:04 +0100

    Keep input component for future use

---
+++ src/components/Input.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const Input = ({
+  name,
+  value,
+  placeholder,
+  handleChange,
+  className,
+}) => {
+  return (
+    <>
+      <input
+        onChange={(e) => handleChange(e)}
+        placeholder={placeholder}
+        value={value}
+        className={className}
+        name={name}
+        type="text"
+      />
+    </>
+  );
+};
+
+Input.propTypes = {
+  name: propTypes.string.isRequired,
+  value: propTypes.string.isRequired,
+  placeholder: propTypes.string.isRequired,
+  handleChange: propTypes.func.isRequired,
+  className: propTypes.string.isRequired,
+};
+
+export default Input;
