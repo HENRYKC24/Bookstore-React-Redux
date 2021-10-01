879bdcb743e4ca0cb28b92788d902883a7a77801
HENRY-KC <henrykc24@yahoo.com>
2021-10-01 12:21:46 +0100

    Remove comments

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,25 +2,58 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import propTypes from 'prop-types';
 import { removeFromServer } from '../redux/books/books';
-import BookPropLi from './BookPropLi';
+// import BookPropLi from './BookPropLi';
 
-const Book = ({
-  title,
-  category,
-  id,
-}) => {
-  const data = [
-    title,
-    category,
-  ];
+const Book = ({ title, category, id }) => {
+  // const data = [
+  //   title,
+  //   category,
+  // ];
+  const [title2, author, completed, chapters, currChapter] = title.split('<*)$!?^|^?!$(*>');
   const dispatch = useDispatch();
   const remove = () => dispatch(removeFromServer(id));
   return (
-    <div>
-      <ul>
-        {data.map((item, index) => index < 4 && <BookPropLi key={Math.random()} item={item} />)}
-      </ul>
-      <button type="button" onClick={remove}>Remove</button>
+    <div className="book">
+      <div className="part1">
+        <div className="part1-first">
+          <div className="category">{category}</div>
+          <div className="title">{title2}</div>
+          <div className="author">{author}</div>
+        </div>
+        <div className="part1-second">
+          <button className="comment-button" type="button">
+            Comment
+          </button>
+          |
+          <button className="remove-button" type="button" onClick={remove}>
+            Remove
+          </button>
+          |
+          <button className="edit-button" type="button">
+            Edit
+          </button>
+        </div>
+      </div>
+      <div className="part2And3">
+        <div className="part2">
+          <svg className="svg" width="150" height="150">
+            <circle className="circle-back" cx="50" cy="80" r="40" />
+            <circle className="circle-front" cx="50" cy="80" r="40" />
+          </svg>
+          <div>
+            <div className="percent">{completed}</div>
+            <div className="completed">completed</div>
+          </div>
+        </div>
+        <div className="pipe" />
+        <div className="part3">
+          <div className="current-chapter">Current Chapter</div>
+          <div className="chapter">
+            <span className="capterXOfY">{`Chapter ${currChapter} of ${chapters}`}</span>
+          </div>
+          <button className="update-button" type="button">Update Progress</button>
+        </div>
+      </div>
     </div>
   );
 };
